9526e6042827d7b1420c09f70da0eb37d45a1ba4
debug-ing <mahdi.bl4ck@gmail.com>
2024-12-18 15:24:46 +0330

    Init

---
+++ main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+
+data = pd.read_csv("height_weight_data.csv")
+
+X = data[["height", "weight"]]
+y = data["status"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+
+k = 3
+model = KNeighborsClassifier(n_neighbors=k)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
+
+
+height = float(input("Enter height (cm): "))
+weight = float(input("Enter weight (kg): "))
+
+new_data = pd.DataFrame([[height, weight]], columns=["height", "weight"])
+
+new_prediction = model.predict(new_data)
+print(f"\nPrediction for new data (height={height}, weight={weight}): {new_prediction[0]}")
